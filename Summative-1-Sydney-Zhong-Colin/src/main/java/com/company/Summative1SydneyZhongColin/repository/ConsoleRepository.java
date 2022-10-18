eac2043ae082a551dbd36b6e73a09dd0bc53870e
Sydney Mercier <sydney.mercier91@gmail.com>
2022-10-18 10:32:29 -0500

    updated console repository

--- Summative-1-Sydney-Zhong-Colin/src/main/java/com/company/Summative1SydneyZhongColin/repository/ConsoleRepository.java
+++ Summative-1-Sydney-Zhong-Colin/src/main/java/com/company/Summative1SydneyZhongColin/repository/ConsoleRepository.java
@@ -5,6 +5,11 @@ import com.company.Summative1SydneyZhongColin.model.Console;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ConsoleRepository extends JpaRepository<Console, Integer> {
+
+    List<Console> findAllConsolesByManufacturer(String manufacturer);
+
 }
