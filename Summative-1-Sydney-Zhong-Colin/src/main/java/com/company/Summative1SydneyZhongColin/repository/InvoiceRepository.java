e40439319a05e0af0cc06954e3aff12cc88b443d
Colin Mosi Cargile <cargile10@gmail.com>
2022-10-19 10:43:41 -0400

    created invoice repository and updated tshirt controller

---
+++ Summative-1-Sydney-Zhong-Colin/src/main/java/com/company/Summative1SydneyZhongColin/repository/InvoiceRepository.java
@@ -0,0 +1,15 @@
+package com.company.Summative1SydneyZhongColin.repository;
+
+
+import com.company.Summative1SydneyZhongColin.model.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
+
+    List<Invoice> findByName(String name);
+}
