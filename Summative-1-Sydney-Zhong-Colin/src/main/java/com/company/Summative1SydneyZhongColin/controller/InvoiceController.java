18858f26128dc499c6bae3c4f13feedce3a2792c
Sydney Mercier <sydney.mercier91@gmail.com>
2022-10-19 11:31:50 -0500

    added invoice controller and service layer

---
+++ Summative-1-Sydney-Zhong-Colin/src/main/java/com/company/Summative1SydneyZhongColin/controller/InvoiceController.java
@@ -0,0 +1,46 @@
+package com.company.Summative1SydneyZhongColin.controller;
+
+import com.company.Summative1SydneyZhongColin.model.Invoice;
+import com.company.Summative1SydneyZhongColin.repository.InvoiceRepository;
+import com.company.Summative1SydneyZhongColin.service.ServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/invoice")
+public class InvoiceController {
+
+    @Autowired
+    private ServiceLayer serviceLayer;
+
+    // create
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Invoice createInvoice(@RequestBody Invoice invoice) {
+        return serviceLayer.saveInvoice(invoice);
+    }
+
+    // get
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Invoice getInvoiceById(@PathVariable Integer id) {
+        return serviceLayer.findInvoiceById(id);
+    }
+
+    // get all
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public List<Invoice> getAllInvoices() {
+        return serviceLayer.findAllInvoices();
+    }
+
+    // get by customer name
+    @GetMapping("/customer/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Invoice> getInvoicesByCustomerName(@PathVariable String name) {
+        return serviceLayer.findInvoicesByCustomerName(name);
+    }
+}
--- Summative-1-Sydney-Zhong-Colin/src/main/java/com/company/Summative1SydneyZhongColin/service/ServiceLayer.java
+++ Summative-1-Sydney-Zhong-Colin/src/main/java/com/company/Summative1SydneyZhongColin/service/ServiceLayer.java
@@ -1,7 +1,131 @@
 package com.company.Summative1SydneyZhongColin.service;
 
+import com.company.Summative1SydneyZhongColin.model.Console;
+import com.company.Summative1SydneyZhongColin.model.Game;
+import com.company.Summative1SydneyZhongColin.model.Invoice;
+import com.company.Summative1SydneyZhongColin.model.T_Shirt;
+import com.company.Summative1SydneyZhongColin.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ServiceLayer {
+
+    private ConsoleRepository consoleRepository;
+    private GameRepository gameRepository;
+    private T_ShirtRepository tShirtRepository;
+    private InvoiceRepository invoiceRepository;
+    private ProcessingFeeRepository processingFeeRepository;
+    private SaleTaxRepository saleTaxRepository;
+
+    @Autowired
+    public ServiceLayer(ConsoleRepository consoleRepository, GameRepository gameRepository, T_ShirtRepository tShirtRepository, InvoiceRepository invoiceRepository, ProcessingFeeRepository processingFeeRepository, SaleTaxRepository saleTaxRepository) {
+        this.consoleRepository = consoleRepository;
+        this.gameRepository = gameRepository;
+        this.tShirtRepository = tShirtRepository;
+        this.invoiceRepository = invoiceRepository;
+        this.processingFeeRepository = processingFeeRepository;
+        this.saleTaxRepository = saleTaxRepository;
+    }
+
+    @Transactional
+    public Invoice saveInvoice(Invoice invoice) {
+        // check that invoice quantity is greater than zero
+        if (invoice.getQuantity() <= 0) {
+            throw new IllegalArgumentException("Quantity must be a positive number");
+        }
+
+        Double subtotal = invoice.getUnitPrice() * invoice.getQuantity();
+        Double tax = saleTaxRepository.findByState(invoice.getState()).getRate();
+        Double processingFee = processingFeeRepository.findByProductType(invoice.getItemType()).getFee();
+
+        if (invoice.getQuantity() > 10) {
+            processingFee += 15.49;
+        }
+
+        Double total = subtotal + processingFee + (subtotal * tax);
+
+        // limit to 2 decimal points by rounding up
+        subtotal = new BigDecimal(subtotal).setScale(2, RoundingMode.HALF_UP).doubleValue();
+        total = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
+
+        invoice.setProcessingFee(processingFee);
+        invoice.setTax(tax);
+        invoice.setSubtotal(subtotal);
+        invoice.setTotal(total);
+
+        // throw error if total is over $999.99
+        if (total > 999.99) {
+            throw new IllegalArgumentException("Database cannot handle invoices over 999.99");
+        }
+
+        // decrement invoice quantity from item quantity
+        switch (invoice.getItemType()) {
+            case "Consoles":
+                Optional<Console> console = consoleRepository.findById(invoice.getItemId());
+                if (console.isPresent()) {
+                    Console tempConsole = console.get();
+                    if (tempConsole.getQuantity() < invoice.getQuantity()) {
+                        throw new IllegalArgumentException("Invoice quantity is greater than remaining stock");
+                    } else {
+                        tempConsole.setQuantity(tempConsole.getQuantity() - invoice.getQuantity());
+                        consoleRepository.save(tempConsole);
+                    }
+                } else {
+                    throw new NotFoundException("console not found");
+                }
+                break;
+            case "Games":
+                Optional<Game> game = gameRepository.findById(invoice.getItemId());
+                if (game.isPresent()) {
+                    Game tempGame = game.get();
+                    if (tempGame.getQuantity() < invoice.getQuantity()) {
+                        throw new IllegalArgumentException("Invoice quantity is greater than remaining stock");
+                    } else {
+                        tempGame.setQuantity(tempGame.getQuantity() - invoice.getQuantity());
+                        gameRepository.save(tempGame);
+                    }
+                } else {
+                    throw new NotFoundException("game not found");
+                }
+                break;
+            case "T-shirts":
+                Optional<T_Shirt> tshirt = tShirtRepository.findById(invoice.getItemId());
+                if (tshirt.isPresent()) {
+                    T_Shirt tempTshirt = tshirt.get();
+                    if (tempTshirt.getQuantity() < invoice.getQuantity()) {
+                        throw new IllegalArgumentException("Invoice quantity is greater than remaining stock");
+                    } else {
+                        tempTshirt.setQuantity(tempTshirt.getQuantity() - invoice.getQuantity());
+                        tShirtRepository.save(tempTshirt);
+                    }
+                } else {
+                    throw new NotFoundException("t-shirt not found");
+                }
+                break;
+            default:
+                throw new NotFoundException("invalid item type");
+        }
+
+        return invoiceRepository.save(invoice);
+    }
+
+    public Invoice findInvoiceById(Integer id) {
+        return invoiceRepository.findById(id).orElse(null);
+    }
+
+    public List<Invoice> findAllInvoices() {
+        return invoiceRepository.findAll();
+    }
+
+    public List<Invoice> findInvoicesByCustomerName(String name) {
+        return invoiceRepository.findByName(name);
+    }
 }
